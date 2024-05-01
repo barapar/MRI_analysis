7ce51f007059c9ad1666678790422b39f1e8ed1f
Elizaveta Parfenova <parfenova.ev.2610@gmail.com>
2024-05-01 13:55:05 +0800

    adjusted imports

--- create_participants_tsv_file.py
+++ create_participants_tsv_file.py
@@ -1,16 +1,16 @@
 '''
 to run fMRIPrep, create participants.tsv
-it contains on column named 'participant_id'
+it should contain one column named 'participant_id'
 one row = one int
 ex.: for "sub-01" write 1
 '''
-# the path to the output data folder
-out_dir = '/home/lisz/Desktop/Time_project/time_fmri/data'
+from os.path import join as opj
 
-participants_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                     11, 12, 13, 15, 16, 17, 18, 19,
-                     21, 22, 23, 24, 25, 26, 27, 28,
-                     30, 31, 32, 33]
+# the path to the output data folder
+out_dir = opj('/', 'home', 'lisz', 'Desktop', 'Time_project', 'time_fmri', 'data')
+
+participants_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 15, 16, 17, 18, 19,
+                     21, 22, 23, 24, 25, 26, 27, 28, 30, 31, 32, 33]
 
 # open the file, if it does not exist it will be created
 with open(out_dir + '/participants.tsv', 'w') as f:
